8cd82a1b30f8e8b12f4f3d26b3034b8a4d5153c7
mathias <mb@dotcomet.de>
2016-11-13 17:42:49 +0100

    added statusbar style on startup

---
+++ client/startup.js
@@ -0,0 +1,13 @@
+Meteor.startup(function(){
+  if(Meteor.isCordova && cordova && cordova.platformId === 'ios'){
+    try{
+      StatusBar.styleDefault();
+
+      setTimeout(function(){
+        StatusBar.styleDefault();
+      }, 1000)
+    }catch(e){
+      console.error(e),
+    }
+  }
+})
